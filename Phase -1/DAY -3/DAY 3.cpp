f0e1a552a1fc3ab4daeb67066a932adefba59083
Mayank08MayankShukla08 <shuklamayank015@gmail.com>
2026-05-02 09:50:41 +0530

    while loop

--- Phase -1/DAY -3/DAY 3.cpp
+++ Phase -1/DAY -3/DAY 3.cpp
@@ -21,3 +21,31 @@ int main(){
         
     return 0;
 }
+
+
+// problem -3 --print 10-1 using a while loop 
+#include <iostream>
+using namespace std;
+int main(){
+    int i= 1;
+    while (i<= 10){
+        cout << i << " ";
+        i++;
+    }
+        
+    return 0;
+}
+
+
+// problem -4 --print 1-10 using a while loop 
+#include <iostream>
+using namespace std;
+int main(){
+    int i= 10;
+    while (i>= 1){
+        cout << i << " ";
+        i--;
+    }
+        
+    return 0;
+}
