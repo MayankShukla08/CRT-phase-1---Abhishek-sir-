9b9231e7c8fbf2125f31b04fa7761c7a0fac1dab
Mayank08MayankShukla08 <shuklamayank015@gmail.com>
2026-05-04 09:54:48 +0530

    day-4 started with loops and pattterns

---
+++ Phase -1/DAY -4/DAY 4.cpp
@@ -0,0 +1,84 @@
+// Day -4
+// Problem -1 -- 1 to 10 ,10 number of times
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //number of times to print the pattern 
+   for (int i = 1; i <= 10; i++ ) {
+       
+       //this defines what to print in the pattern 
+       for (int j = 1; j<=10;  j++){
+           cout << j << " ";
+   }
+    cout << "\n";
+   }
+
+    return 0;
+}
+
+
+// Problem 2 
+// Print a Solid square 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // number of times to print the pattern
+    for(int i = 1; i <= 10; i++) {
+        // this defines what to print in the pattern
+        for(int j = 1; j <= 10; j++){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+
+
+// Problem 3 
+// Print a right angled triangle 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // number of times to print the pattern
+    for(int i = 1; i <= 6; i++) {
+        // this defines what to print in the pattern
+        for(int j = 1; j <= i; j++){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+                                        // OR
+
+
+// Problem 3 
+// Print a right angled triangle 
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n ;
+    cin >> n;
+    
+    // number of times to print the pattern
+    for(int i = 1; i <= n; i++) {
+        // this defines what to print in the pattern
+        for(int j = 1; j <= i; j++){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
