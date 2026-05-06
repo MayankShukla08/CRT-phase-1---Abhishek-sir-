4e3c2b24b78512056e931680dbd413d1cc01266f
Mayank08MayankShukla08 <shuklamayank015@gmail.com>
2026-05-06 11:12:39 +0530

    function overloading | default argument

--- Phase -1/DAY -6/DAY 6.cpp
+++ Phase -1/DAY -6/DAY 6.cpp
@@ -59,3 +59,48 @@ int main() {
   updatebyReference(original);
   original << original;
 }
+
+
+// practice problems -- 4
+// coffee billing problem
+
+#include <iostream>
+using namespace std;
+
+int billing(int a){
+    return a*30;
+}
+int main (){
+    int coffee = 3;
+    cout << billing(coffee);
+}
+
+
+
+// practice problems --5
+// samrt ac remote // function overloading // default argument
+
+#include <iostream>
+using namespace std;
+
+void setTemprature(int temp = 22){
+    cout << "Temprature set to " << temp  << "°C" << "\n";
+}
+
+void setTemprature(double temp){
+    cout << "Temprature set to " << temp  << "°C" << "\n";
+}
+void setTemprature(string mode) {
+    if(mode == "ECO"){
+        cout << "Temprature set to 24°C " << "\n";
+    } else if(mode == "TURBO") {
+        cout << "Temprature set to 18°C " << "\n";
+    }
+}
+
+int main() {
+    setTemprature(); // default value -- 22
+    setTemprature(20.5); // manually setting the temprature
+    setTemprature("ECO"); // set temprature to 24
+    setTemprature("TURBO"); // setTemprature to 18
+}
