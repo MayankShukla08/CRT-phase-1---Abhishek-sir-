dae99b8c9794dffeb9fc6aec6d09daf8cb4beb43
Mayank08MayankShukla08 <shuklamayank015@gmail.com>
2026-05-06 09:54:12 +0530

    udpate by reference and value

--- Phase -1/DAY -6/DAY 6.cpp
+++ Phase -1/DAY -6/DAY 6.cpp
@@ -19,3 +19,43 @@ int main() {
     swapNumber(&a,&b);
     cout << a << " " << b;  
 }
+
+
+// Problem -- 2
+// Default Argument
+
+#include <iostream>
+using namespace std;
+
+int area (int a, int b =5){
+    return a*b;
+    
+}
+int main() {
+   cout << area (10);
+}
+
+
+/ Problem -- 3
+// Call by value -- Photocopy
+// Call by Reference -- Original copy 
+
+#include <iostream>
+using namespace std;
+
+void updatebyValue(int a){
+    a=500;
+}
+void updatebyReference(int &a){
+    a=500;  
+}
+
+
+int main() {
+  int original = 100;
+  
+  updatebyValue(original);
+  cout << original;
+  updatebyReference(original);
+  original << original;
+}
